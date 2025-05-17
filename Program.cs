9ad9bb9cb74141f259887dc6160516be413d5fbc
alvardroid <alvardroid@gmail.com>
2025-05-18 00:03:55 +0200

    Test Crear Tabla

--- Program.cs
+++ Program.cs
@@ -27,6 +27,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<UserContext>();
+    db.Database.EnsureCreated();
+}
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
